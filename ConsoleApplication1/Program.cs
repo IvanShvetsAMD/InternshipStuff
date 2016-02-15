3879ec8c2e5632d3e9b64bc63b77650d8c82b394
Ivan Shvets <shvetsivan92@gmail.com>
2016-02-15 17:02:17 +0200

    bla lba

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -13,7 +13,6 @@ namespace ConsoleApplication1
 
             gen.OutputVoltage = 0;
             gen.OutputCurrent = 0;
-            
 
             ep.Voltage = 3;
             ep.Current = 3;
@@ -30,7 +29,7 @@ namespace ConsoleApplication1
             //gen = new Generator(new ElectricParameters(0, 3));
             //ep = new ElectricParameters(0, 3);
 
-            gen.OutputVoltage = 0;
+            gen.OutputVoltage = 10;
             gen.OutputCurrent = 0;                       
 
             ep.Voltage = 3;
@@ -44,95 +43,101 @@ namespace ConsoleApplication1
 
             #region 
 
-            //List<Generator> Generators = new List<Generator>();
-            //Random rnd = new Random();
-            //for (int i = 0; i < 5; i++)
-            //    Generators.Add(new Generator(rnd.Next(0, 10), rnd.Next(0, 10) + rnd.NextDouble()));
+            List<Generator> Generators = new List<Generator>();
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+                Generators.Add(new Generator(rnd.Next(0, 10), rnd.Next(0, 10) + rnd.NextDouble()));
 
-            //Printer.Print(Generators);
+            Printer.Print(Generators);
 
-            //ElectricParameters ep1 = new ElectricParameters(1, 5.5);
-            //ElectricParameters ep2 = new ElectricParameters(3, 0.4);
-            //Generators[2] = new Generator(ep1);
-            //Generators[4] = new Generator(ep2);
+            ElectricParameters ep1 = new ElectricParameters(1, 5.5);
+            ElectricParameters ep2 = new ElectricParameters(3, 0.4);
+            Generators[2] = new Generator(ep1);
+            Generators[4] = new Generator(ep2);
 
-            //Printer.Print(Generators);
+            Printer.Print(Generators);
 
-            ////ref prameter
-            //Console.WriteLine("\nref");
-            //Generator gen1 = new Generator(rnd.Next(0, 10), rnd.Next(0, 10) + rnd.NextDouble());
-            //ElectricParameters ep3 = new ElectricParameters(rnd.Next(0, 10), rnd.Next(0, 10) + rnd.NextDouble());
-            //Console.WriteLine("\nBefore:");
-            //Printer.Print(gen1, ep3);
-            //StopGenerator(ref gen1, ref ep3);
-            //Console.WriteLine("\nAfter:");
-            //Printer.Print(gen1, ep3);
+            //ref prameter
+            Console.WriteLine("\nref");
+            Generator gen1 = new Generator(rnd.Next(0, 10), rnd.Next(0, 10) + rnd.NextDouble());
+            ElectricParameters ep3 = new ElectricParameters(rnd.Next(0, 10), rnd.Next(0, 10) + rnd.NextDouble());
+            Console.WriteLine("\nBefore:");
+            Printer.Print(gen1, ep3);
+            StopGenerator(ref gen1, ref ep3);
+            Console.WriteLine("\nAfter:");
+            Printer.Print(gen1, ep3);
 
-            ////out parameter
-            //Console.WriteLine("\nout");
-            //Generator gen2 = new Generator(ep1);
-            //ElectricParameters ep4 = new ElectricParameters(rnd.Next(0, 10), rnd.Next(0, 10) + rnd.NextDouble());
-            //Console.WriteLine("\nBefore:");
-            //Printer.Print(gen2, ep4);
-            //MaxOutGenerator(out gen2, out ep4);
-            //Console.WriteLine("\nAfter:");
-            //Printer.Print(gen2, ep4);
+            //out parameter
+            Console.WriteLine("\nout");
+            Generator gen2 = new Generator(ep1);
+            ElectricParameters ep4 = new ElectricParameters(rnd.Next(0, 10), rnd.Next(0, 10) + rnd.NextDouble());
+            Console.WriteLine("\nBefore:");
+            Printer.Print(gen2, ep4);
+            MaxOutGenerator(out gen2, out ep4);
+            Console.WriteLine("\nAfter:");
+            Printer.Print(gen2, ep4);
 
-            ////passing by value
-            //Console.WriteLine("\nby value");
-            //gen1 = new Generator(rnd.Next(0, 10), rnd.Next(0, 10) + rnd.NextDouble());
-            //ElectricParameters ep5 = new ElectricParameters(rnd.Next(0, 10), rnd.Next(0, 10) + rnd.NextDouble());
-            //Console.WriteLine("\nBefore:");
-            //Printer.Print(gen1, ep5);
-            //RestartGenerator(gen1, ep5);
-            //Console.WriteLine("\nAfter:");
-            //Printer.Print(gen1, ep5);
+            //passing by value
+            Console.WriteLine("\nby value");
+            gen1 = new Generator(rnd.Next(0, 10), rnd.Next(0, 10) + rnd.NextDouble());
+            ElectricParameters ep5 = new ElectricParameters(rnd.Next(0, 10), rnd.Next(0, 10) + rnd.NextDouble());
+            Console.WriteLine("\nBefore:");
+            Printer.Print(gen1, ep5);
+            RestartGenerator(gen1, ep5);
+            Console.WriteLine("\nAfter:");
+            Printer.Print(gen1, ep5);
 
 
-            ////boxing and unboxing
-            //char c1 = 'b';
-            //object o = c1;
-            //char c2 = (char) o;
-            //Console.WriteLine("\nBoxing a char into an object and unboxing it.");
-            //Console.WriteLine("As a char: " + c1);
-            //Console.WriteLine("As an object: " + o);
-            //Console.WriteLine("As a char again: " + c2);
+            //boxing and unboxing
+            char c1 = 'b';
+            object o = c1;
+            char c2 = (char)o;
+            Console.WriteLine("\nBoxing a char into an object and unboxing it.");
+            Console.WriteLine("As a char: " + c1);
+            Console.WriteLine("As an object: " + o);
+            Console.WriteLine("As a char again: " + c2);
 
             #endregion
 
 
-            Angle angle1 = new Angle(4020);
-            Console.WriteLine(Angle.ToSeconds(angle1));
-            Angle angle2 = new Angle(1, 61, 0);
-            Console.WriteLine(Angle.ToSeconds(angle2));
+            //Angle angle1 = new Angle(4020);
+            //Console.WriteLine(Angle.ToSeconds(angle1));
+            //Angle angle2 = new Angle(1, 61, 0);
+            //Console.WriteLine(Angle.ToSeconds(angle2));
 
 
-            Console.WriteLine("\nAngle1: " + Angle.ToSeconds(angle1) + "\nAngle2: " + Angle.ToSeconds(angle2));
-            Console.WriteLine("\nAngle1: " + angle1.ToString() + "\nAngle2: " + angle2.ToString());
+            //Console.WriteLine("\nAngle1: " + Angle.ToSeconds(angle1) + "\nAngle2: " + Angle.ToSeconds(angle2));
+            //Console.WriteLine("\nAngle1: " + angle1 + "\nAngle2: " + angle2);
 
-            Console.WriteLine("\nangle2 = angle1 + angle2");
-            angle2 = angle1 + angle2;
-            Console.WriteLine("Angle1: " + Angle.ToSeconds(angle1) + "\nAngle2: " + Angle.ToSeconds(angle2));
-            Console.WriteLine("Angle1: " + angle1.ToString() + "\nAngle2: " + angle2.ToString());
+            //Console.WriteLine("\nangle2 = angle1 + angle2");
+            //angle2 = angle1 + angle2;
+            //Console.WriteLine("Angle1: " + Angle.ToSeconds(angle1) + "\nAngle2: " + Angle.ToSeconds(angle2));
+            //Console.WriteLine("Angle1: " + angle1 + "\nAngle2: " + angle2);
 
-            Console.WriteLine("\nangle2 = angle2 - angle1");
-            angle2 = angle2 - angle1;
-            Console.WriteLine("Angle1: " + Angle.ToSeconds(angle1) + "\nAngle2: " + Angle.ToSeconds(angle2));
-            Console.WriteLine("Angle1: " + angle1.ToString() + "\nAngle2: " + angle2.ToString());
-            Console.WriteLine("Equal: " + (angle2 == angle1));
+            //Console.WriteLine("\nangle2 = angle2 - angle1");
+            //angle2 = angle2 - angle1;
+            //Console.WriteLine("Angle1: " + Angle.ToSeconds(angle1) + "\nAngle2: " + Angle.ToSeconds(angle2));
+            //Console.WriteLine("Angle1: " + angle1 + "\nAngle2: " + angle2);
+            //Console.WriteLine("Equal: " + (angle2 == angle1));
 
-            Console.WriteLine("\nangle2 -= angle1");
-            angle2 -= angle1;
-            Console.WriteLine("Angle1: " + Angle.ToSeconds(angle1) + "\nAngle2: " + Angle.ToSeconds(angle2));
-            Console.WriteLine("Angle1: " + angle1.ToString() + "\nAngle2: " + angle2.ToString());
+            //Console.WriteLine("\nangle2 -= angle1");
+            //angle2 -= angle1;
+            //Console.WriteLine("Angle1: " + Angle.ToSeconds(angle1) + "\nAngle2: " + Angle.ToSeconds(angle2));
+            //Console.WriteLine("Angle1: " + angle1 + "\nAngle2: " + angle2);
 
-            Console.WriteLine("\nAngle1: " + Angle.ToSeconds(angle1) + "\nAngle2: " + Angle.ToSeconds(angle2));
-            Console.WriteLine("\nAngle1: " + angle1.ToString() + "\nAngle2: " + angle2.ToString());
-            Console.WriteLine("Greater (angle1 > angle2): " + (angle1 > angle2));
-            Console.WriteLine("Lesser (angle1 < angle2): " + (angle1 < angle2));
-            Console.WriteLine("Greater or equal (angle1 >= angle2): " + (angle1 >= angle2));
-            Console.WriteLine("Less or equal (angle1 <= angle2): " + (angle1 <= angle2));
-            Console.WriteLine("Equal (angle1 == angle2): " + (angle2 == angle1));
+            //Console.WriteLine("\nAngle1: " + Angle.ToSeconds(angle1) + "\nAngle2: " + Angle.ToSeconds(angle2));
+            //Console.WriteLine("\nAngle1: " + angle1 + "\nAngle2: " + angle2);
+            //Console.WriteLine("Greater (angle1 > angle2):\t\t " + (angle1 > angle2));
+            //Console.WriteLine("Lesser (angle1 < angle2):\t\t " + (angle1 < angle2));
+            //Console.WriteLine("Greater or equal (angle1 >= angle2):\t " + (angle1 >= angle2));
+            //Console.WriteLine("Less or equal (angle1 <= angle2):\t " + (angle1 <= angle2));
+            //Console.WriteLine("Equal (angle1 == angle2):\t\t " + (angle2 == angle1));
+
+
+            //Console.WriteLine("\nangle2 = angle1 * angle2");
+            //angle2 = angle1*2;
+            //Console.WriteLine("Angle1: " + Angle.ToSeconds(angle1) + "\nAngle2: " + Angle.ToSeconds(angle2));
+            //Console.WriteLine("Angle1: " + angle1 + "\nAngle2: " + angle2);
         }
     }
 }
