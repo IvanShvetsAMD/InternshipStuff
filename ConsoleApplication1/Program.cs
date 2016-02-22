865f51544045b2d1cdbfcca69f1f215e118e05bc
Ivan <vini_puh@hotmail.com>
2016-02-22 23:27:40 +0200

    smth

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -312,7 +312,7 @@ namespace ConsoleApplication1
                 Console.WriteLine(o.GetType().Name);
             }
 
-            Console.WriteLine("LINQ");
+            Console.WriteLine("\nLINQ");
             foreach (var o in trylinq(stuff))
             {
                 Console.WriteLine(o.GetType().Name);
