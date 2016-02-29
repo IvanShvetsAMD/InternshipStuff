cde1b96b21193c8cb67aa68fbe94e3719da88c1c
Ivan Shvets <shvetsivan92@gmail.com>
2016-02-29 11:22:34 +0200

    -Added some sort of log (still unclear if it works);

---
+++ Logger/LogEventArgs.cs
@@ -0,0 +1,21 @@
+﻿namespace Logger
+{
+    public class LogEventArgs
+    {
+        public readonly Logger Log;
+        public readonly string LogText;
+
+        public LogEventArgs(Logger a = null, string b = null)
+        {
+            Log = a;
+            LogText = b;
+        }
+
+        public LogEventArgs(string a)
+        {
+            LogText = a;
+            if (Log != null)
+                Log.AddToLog(a);
+        }
+    }
+}
---
+++ Logger/Logger.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public delegate void LogChangedDelegate(LogEventArgs e);
+    public class Logger
+    {
+        private string Log;
+        private string directory = "D:\\";
+        private string filename = "Voting Server Log";
+
+        public string Directory
+        {
+            private get { return directory; }
+            set
+            {
+                AddToLog("Log file directory changed to " + value);
+                directory = value;
+            }
+        }
+
+        public string FileName
+        {
+            private get { return filename; }
+            set
+            {
+                AddToLog("Log file name changed to '" + value + ".txt'");
+                filename = value;
+            }
+        }
+
+        public LogChangedDelegate LogChangedEvent;
+
+        public Logger() { }
+
+        public void AddToLog(string a)
+        {
+            Log += Environment.NewLine + "[" + DateTime.Now + "] " + a;
+
+            LogChangedDelegate handler = LogChangedEvent;
+            handler?.Invoke(new LogEventArgs(this));
+        }
+
+        public void ExportToFile()
+        {
+            AddToLog("Log exported to file (" + Directory + "\\" + FileName + ".txt)");
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(Directory + "\\" + FileName + ".txt", true))
+                file.WriteLine(ToString());
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Log ({0}): {1}", DateTime.Now, Log);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ToString() == obj.ToString();
+        }
+    }
+}
