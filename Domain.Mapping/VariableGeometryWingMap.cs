f63ba3efd1d7cd516534124ff5c7bcd2080a1abd
Ivan Shvets <shvetsivan92@gmail.com>
2016-03-28 09:17:06 +0300

    -Wing classes remapped to "table-per-concrete-class" pattern;

--- Domain.Mapping/VariableGeometryWingMap.cs
+++ Domain.Mapping/VariableGeometryWingMap.cs
@@ -10,6 +10,10 @@ namespace Domain.Mapping
     {
         public VariableGeometryWingMap()
         {
+            Id(x => x.Id);
+            Map(x => x.FuelCapacity).Not.Nullable();
+            Map(x => x.RootThickness).Not.Nullable();
+            Map(x => x.WingAngle).Not.Nullable();
             Map(x => x.MaxBackSweepAngle).Not.Nullable();
             Map(x => x.MaxForwardSweepAngle).Not.Nullable();
         }
