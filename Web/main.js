847976957382b86295629a00b39557635a76dd26
Ivan Shvets <shvetsivan92@gmail.com>
2016-04-07 17:30:34 +0300

    -WEB;

--- Web/main.js
+++ Web/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
-function SkewBody(element) {
-    element.style = "transform: skewX(35deg;)";
+function SkewBody(ID) {    
+    var body = document.getElementsByTagName("body");
+    body[0].style="transform: skewX(35deg);";    
+}
+
+function Run_Button_Run!(ID){
+    //var button = document.getElementById(ID);
+    //button.location.hei
+    ID.location.width *= 2;
+}
+
+function Highlight(element){
+    //element.style.backgroundColor = "#99999999"
+    alert("dsfdfdsfs");
 }
